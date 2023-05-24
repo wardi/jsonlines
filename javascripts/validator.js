d3991127cf111700230ed76188406e2dbc1aab43
Baptiste Pernet <pernetmu@gmail.com>
2023-05-24 11:26:16 -0400

    Web validator for jsonlines (#70)
    * Web validator for jsonlines
    
    * Add external js and css as vendor
    
    * Add javacript mode to CodeMirror
    
    ---------
    
    Co-authored-by: Baptiste Pernet <bpernet@stellar.aero>

---
+++ javascripts/validator.js
@@ -0,0 +1,46 @@
+/* VALIDATOR */
+
+$(document).ready(() => {
+  // CodeMirror editor
+  let editor = CodeMirror.fromTextArea(jQuery('.validator textarea')[0], {
+    lineNumbers: true,
+    theme: 'abbott',
+    mode: 'javascript',
+  });
+  
+  // clear button
+  jQuery('.validator input[type="button"][value="clear"]').click(() => {
+    editor.setValue('');
+  });
+
+  // validate button
+  jQuery('.validator input[type="button"][value="validate"]').click(() => {
+    let jsonl = editor.getValue();
+    let lines = jsonl.split('\n');
+    let errors = [];
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i]
+      if (i !== lines.length - 1 || lines !== '') {
+        try {
+          JSON.parse(line);
+        } catch (e) {
+          if (e instanceof SyntaxError) {
+            errors.push(`line ${i + 1}: error ${e}\n`);
+          } else {
+            throw e;
+          }
+        }
+      }
+    }
+
+    let result = jQuery('.validator div.result ul');
+    result.empty();
+    if (0 < errors.length) {
+      errors.forEach((error) => {
+        result.append(`<li>${error}</li>`);
+      });
+    } else {
+      result.append('valid jsonl')
+    }
+  });
+});
